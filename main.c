4ffe2efc289fb9c39de2c242d48a205629d791c1
mihawkcola <zockerelf@gmail.com>
2020-06-02 14:26:10 +0200

    ufd

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ int main (int argc, char *argv[])
     strcat(a,cDelay);
     strcat(a,"\0");
     parameter[0]=a;
-    printf("\nDelay%s\n",parameter[0]);
+    printf("\n%s\n",parameter[0]);
 
     queue *fifo;
     pthread_t pro;
